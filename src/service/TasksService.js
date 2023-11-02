f254697472a1df974e04d9bfb5ebdd3a4e2d9671
Adrián Gámez <adrian@applicationcare.net>
2023-11-02 12:51:26 +0100

    tasks wip

--- src/service/TasksService.js
+++ src/service/TasksService.js
@@ -1,157 +1,158 @@
-import axiosConfig from '@/service/axiosConfig';
+import axiosConfig from "@/service/axiosConfig";
 
 export const TasksService = {
-    getTasksData() {
-        const tasks = []; 
-        
-        for (let i = 0; i < 100; i++) {
-            const is_open = Math.random() < 0.5;
-            tasks.push({
-                task_number: '469879'+ i,
-                process_number: '687984'+ i,
-                task_type: 'תשאול לקוח',
-                family: 'מוקד',
-                contact: 'שלומי שבת',
-                contact_id: '795'+ i,
-                status: is_open ? 'פתוח' : 'סגור',
-                due_date: '11/12/23',
-                SLA: is_open ? slas[parseInt(Math.random() * 3)] : slas[parseInt(Math.random() * 2 + 3)],
-                owner: 'Israel Israeli',
-                team: 'אגף רישוי ופיקוח (הנדסה)',
-                customer: 'טיילור סוויפט',
-                call: 'כן',
-                open_date: '11/11/22',
-                close_date: is_open ? '':'11/12/23',
-                last_change: '11/11/22 10:23',
-                remarks: 'תוכן של הערה שכתובה יפה מאוד',
-                stage: "קבלת בקשה",
-                is_open: is_open,
-                service_number: '469879'+ i,
-                location: {
-                    house_number: '12',
-                    apartment: '42',
-                    entrance: 'ב',
-                    street: 'שמעון פרס',
-                    city: 'כוכב יאיר',
-                    country: 'ישראל',
-                    zip: '1234567',
-                },
-                site: {
-                    name: 'מועצה דתית בית הכנסת',
-                    contact: 'שלומי שבת',
-                    contact_id: '795'+ i,
-                    contact_role: 'אחראי תברואה',
-                    phone: '054-1234567',
-                    email: 'mailmail@mailmail.com',
-                    type: 'ציבורי',
+  getTasksData() {
+    const tasks = [];
 
-                }
-            });
-        }
-
-        return tasks;
-    },
-
-    getTasksMini() {
-        return Promise.resolve(this.getTasksData().slice(0, 5));
-    },
-
-    getTasksSmall() {
-        return Promise.resolve(this.getTasksData().slice(0, 10));
-    },
-
-    getTasks() {
-        return Promise.resolve(this.getTasksData());
-    },
-
-    getTasksWithOrdersSmall() {
-        return Promise.resolve(this.getTasksWithOrdersData().slice(0, 10));
-    },
-
-    getTasksWithOrders() {
-        return Promise.resolve(this.getTasksWithOrdersData());
-    },
-    getTask(task_number){
-        return Promise.resolve(this.getTasksData().find((task) => task.task_number === task_number));
-    },
-    getTasksFromApi(params) {
-        console.log(params)
-        return axiosConfig.get('/tasks', { params })
-            .then((response) => {
-                console.log(response)
-                const tasks = response.data.data.map((service) => {
-                    return this.mapTask(service);
-                })
-                const totalRecords = response.data.meta.total;
-                console.log(tasks)
-                return { tasks, totalRecords };
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    },
-
-    getTaskFromApi(id) {
-        return axiosConfig.get('/tasks/' + id)
-            .then((response) => {
-                console.log(response)
-                const service = response.data.data;
-                return this.mapTask(service);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-    },
-
-    mapTask(task){
-        const is_open = Math.random() < 0.5;
-        return {
-            task_number: task.id,
-            process_number: '687984',
-            task_type: task.task_type.name,
-            family: 'מוקד',
-            contact: 'שלומי שבת',
-            contact_id: '795',
-            status: 'סגור', //is_open ? 'פתוח' : 'סגור',
-            due_date: '11/12/23',
-            SLA: is_open ? slas[parseInt(Math.random() * 3)] : slas[parseInt(Math.random() * 2 + 3)],
-            owner: 'Israel Israeli',
-            team: 'אגף רישוי ופיקוח (הנדסה)',
-            customer: 'טיילור סוויפט',
-            call: 'כן',
-            open_date: '11/11/22',
-            close_date: is_open ? '':'11/12/23',
-            last_change: '11/11/22 10:23',
-            remarks: 'תוכן של הערה שכתובה יפה מאוד',
-            stage: "קבלת בקשה",
-            is_open: is_open,
-            service_number: '469879',
-            location: {
-                house_number: '12',
-                apartment: '42',
-                entrance: 'ב',
-                street: 'שמעון פרס',
-                city: 'כוכב יאיר',
-                country: 'ישראל',
-                zip: '1234567',
-            },
-            site: {
-                name: 'מועצה דתית בית הכנסת',
-                contact: 'שלומי שבת',
-                contact_id: '795',
-                contact_role: 'אחראי תברואה',
-                phone: '054-1234567',
-                email: 'mailmail@mailmail.com',
-                type: 'ציבורי',
-
-            }
-        }
+    for (let i = 0; i < 100; i++) {
+      const is_open = Math.random() < 0.5;
+      tasks.push({
+        task_number: "469879" + i,
+        process_number: "687984" + i,
+        task_type: "תשאול לקוח",
+        family: "מוקד",
+        contact: "שלומי שבת",
+        contact_id: "795" + i,
+        status: is_open ? "פתוח" : "סגור",
+        due_date: "11/12/23",
+        SLA: is_open
+          ? slas[parseInt(Math.random() * 3)]
+          : slas[parseInt(Math.random() * 2 + 3)],
+        owner: "Israel Israeli",
+        team: "אגף רישוי ופיקוח (הנדסה)",
+        customer: "טיילור סוויפט",
+        call: "כן",
+        open_date: "11/11/22",
+        close_date: is_open ? "" : "11/12/23",
+        last_change: "11/11/22 10:23",
+        remarks: "תוכן של הערה שכתובה יפה מאוד",
+        stage: "קבלת בקשה",
+        is_open: is_open,
+        service_number: "469879" + i,
+        location: {
+          house_number: "12",
+          apartment: "42",
+          entrance: "ב",
+          street: "שמעון פרס",
+          city: "כוכב יאיר",
+          country: "ישראל",
+          zip: "1234567",
+        },
+        site: {
+          name: "מועצה דתית בית הכנסת",
+          contact: "שלומי שבת",
+          contact_id: "795" + i,
+          contact_role: "אחראי תברואה",
+          phone: "054-1234567",
+          email: "mailmail@mailmail.com",
+          type: "ציבורי",
+        },
+      });
     }
 
-    
-    
-    
+    return tasks;
+  },
+
+  getTasksMini() {
+    return Promise.resolve(this.getTasksData().slice(0, 5));
+  },
+
+  getTasksSmall() {
+    return Promise.resolve(this.getTasksData().slice(0, 10));
+  },
+
+  getTasks() {
+    return Promise.resolve(this.getTasksData());
+  },
+
+  getTasksWithOrdersSmall() {
+    return Promise.resolve(this.getTasksWithOrdersData().slice(0, 10));
+  },
+
+  getTasksWithOrders() {
+    return Promise.resolve(this.getTasksWithOrdersData());
+  },
+  getTask(task_number) {
+    return Promise.resolve(
+      this.getTasksData().find((task) => task.task_number === task_number)
+    );
+  },
+  getTasksFromApi(params) {
+    console.log(params);
+    return axiosConfig
+      .get("/tasks", { params })
+      .then((response) => {
+        console.log(response);
+        const tasks = response.data.data.map((service) => {
+          return this.mapTask(service);
+        });
+        const totalRecords = response.data.meta.total;
+        console.log(tasks);
+        return { tasks, totalRecords };
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  getTaskFromApi(id) {
+    return axiosConfig
+      .get("/tasks/" + id)
+      .then((response) => {
+        console.log(response);
+        const service = response.data.data;
+        return this.mapTask(service);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  },
+
+  mapTask(task) {
+    const is_open = Math.random() < 0.5;
+    return {
+      task_number: task.id,
+      process_number: "687984",
+      task_type: task.task_type.name,
+      family: "מוקד",
+      contact: "שלומי שבת",
+      contact_id: "795",
+      status: "סגור", //is_open ? 'פתוח' : 'סגור',
+      due_date: "11/12/23",
+      SLA: is_open
+        ? slas[parseInt(Math.random() * 3)]
+        : slas[parseInt(Math.random() * 2 + 3)],
+      owner: "Israel Israeli",
+      team: "אגף רישוי ופיקוח (הנדסה)",
+      customer: "טיילור סוויפט",
+      call: "כן",
+      open_date: "11/11/22",
+      close_date: is_open ? "" : "11/12/23",
+      last_change: "11/11/22 10:23",
+      remarks: "תוכן של הערה שכתובה יפה מאוד",
+      stage: "קבלת בקשה",
+      is_open: is_open,
+      service_number: "469879",
+      location: {
+        house_number: "12",
+        apartment: "42",
+        entrance: "ב",
+        street: "שמעון פרס",
+        city: "כוכב יאיר",
+        country: "ישראל",
+        zip: "1234567",
+      },
+      site: {
+        name: "מועצה דתית בית הכנסת",
+        contact: "שלומי שבת",
+        contact_id: "795",
+        contact_role: "אחראי תברואה",
+        phone: "054-1234567",
+        email: "mailmail@mailmail.com",
+        type: "ציבורי",
+      },
+    };
+  },
 };
 
-const slas = ['10 ימים', '2 ימים', '3 ימים', 'עמד ביעד', 'הסתיים בחריגה'];
-
+const slas = ["10 ימים", "2 ימים", "3 ימים", "עמד ביעד", "הסתיים בחריגה"];
--- src/views/user/lists/Tasks.vue
+++ src/views/user/lists/Tasks.vue
@@ -1,101 +1,141 @@
 <template>
-    <WMListSubHeader :activeButtons="isAnyRowSelected" :filterLabels="filterLabels" :defaultOption="filterLabels[1]"
-                     entity="task" @new="displayNewForm">
-    </WMListSubHeader>
-    <WMNewEntitySidebar name="newTask" entity="task"  />
-    <div class="table-container mt-5 mx-8 flex-auto overflow-auto">
-        <DataTable lazy v-model:selection="selectedTasks" :rowClass="rowClass" :value="tasks" dataKey="task_number"
-                   tableStyle="min-width: 50rem" class="p-datatable-sm" scrollable scrollHeight="flex" paginator :rows="10"
-                   :first="0" ref="dt" :totalRecords="totalRecords" :loading="loading" @page="onPage($event)">
-            <Column style="width: 40px" selectionMode="multiple"></Column>
-            <!-- Task number -->
-            <Column field="task_number" :header="$t('task.number')">
-                <template #body="slotProps">
-                    <router-link :to="{ name: 'taskDetail', params: { 'id': slotProps.data.task_number } }"
-                                 class="vertical-align-middle">{{ slotProps.data.task_number }}</router-link>
-                </template>
-            </Column>
-            <!-- Service number -->
-            <Column field="process_number" :header="$t('task.service_number')">
-                <template #body="slotProps">
-                    <router-link to="/foo" class="vertical-align-middle">{{ slotProps.data.process_number }}</router-link>
-                </template>
-            </Column>
-            <!-- Task type -->
-            <Column field="task_type" :header="$t('task.type')"></Column>
-            <!-- Task family -->
-            <Column field="family" :header="$t('task.family')"></Column>
-            <!-- Task contact -->
-            <Column field="contact" :header="$t('contact.contact')">
-                <template #body="slotProps">
-                    <router-link to="/foo" class="vertical-align-middle">{{ slotProps.data.contact }}</router-link>
-                </template>
-            </Column>
-            <!-- Task status -->
-            <Column field="status" :header="$t('task.status')"></Column>
-            <!-- Goal date -->
-            <Column field="due_date" :header="$t('task.due_date')"></Column>
-            <!-- SLA -->
-            <Column field="SLA" header="SLA" class="sla">
-                <template #body="slotProps">
-                    <div :class="slaClass(slotProps.data)">
-                        {{ slotProps.data.SLA }}
-                    </div>
-                </template>
-            </Column>
-            <!-- Owner -->
-            <Column field="owner" :header="$t('task.owner')">
-                <template #body="slotProps">
-                    <router-link to="/foo" class="vertical-align-middle">{{ slotProps.data.owner }}</router-link>
-                </template>
-            </Column>
-            <!-- Customer -->
-            <Column field="customer" :header="$t('task.customer')">
-                <template #body="slotProps">
-                    <router-link to="/foo" class="vertical-align-middle">{{ slotProps.data.customer }}</router-link>
-                </template>
-            </Column>
-            <!-- Read -->
-            <Column field="call" :header="$t('task.call')"></Column>
-            <!-- Date opened -->
-            <Column field="open_date" :header="$t('task.open_date')"></Column>
-            <!-- Date closed -->
-            <Column field="close_date" :header="$t('task.close_date')"></Column>
-            <!-- Last changed -->
-            <Column field="last_change" :header="$t('modified_at')"></Column>
-            <!-- Notes -->
-            <Column field="remarks" :header="$t('task.notes')"></Column>
-
-        </DataTable>
-    </div>
+  <WMListSubHeader
+    :activeButtons="isAnyRowSelected"
+    :filterLabels="filterLabels"
+    :defaultOption="filterLabels[1]"
+    entity="task"
+    @new="displayNewForm"
+  />
+  <WMNewEntitySidebar name="newTask" entity="task" />
+  <!-- <pre>{{ tasks }}</pre> -->
+  <div class="table-container mt-5 mx-8 flex-auto overflow-auto">
+    <DataTable
+      lazy
+      v-model:selection="selectedTasks"
+      :rowClass="rowClass"
+      :value="tasks"
+      dataKey="task_number"
+      tableStyle="min-width: 50rem"
+      class="p-datatable-sm"
+      scrollable
+      scrollHeight="flex"
+      paginator
+      :rows="10"
+      :first="0"
+      ref="dt"
+      :totalRecords="totalRecords"
+      :loading="loading"
+      @page="onPage($event)"
+    >
+      <Column style="width: 40px" selectionMode="multiple"></Column>
+      <!-- Task number -->
+      <Column field="task_number" :header="$t('task.number')">
+        <template #body="slotProps">
+          <router-link
+            :to="{
+              name: 'taskDetail',
+              params: { id: slotProps.data.task_number },
+            }"
+            class="vertical-align-middle"
+            >{{ slotProps.data.task_number }}</router-link
+          >
+        </template>
+      </Column>
+      <!-- Service number -->
+      <Column field="process_number" :header="$t('task.service_number')">
+        <template #body="slotProps">
+          <router-link
+            :to="{
+              name: 'serviceDetail',
+              params: { id: slotProps.data.process_number },
+            }"
+            class="vertical-align-middle"
+            >{{ slotProps.data.process_number }}
+          </router-link>
+        </template>
+      </Column>
+      <!-- Task type -->
+      <Column field="task_type" :header="$t('task.type')"></Column>
+      <!-- Task family -->
+      <Column field="family" :header="$t('task.family')"></Column>
+      <!-- Task contact -->
+      <Column field="contact" :header="$t('contact.contact')">
+        <template #body="slotProps">
+          <router-link
+            :to="{
+              name: 'contactDetail',
+              params: { id: slotProps.data.contact_id },
+            }"
+            class="vertical-align-middle"
+          >
+            {{ slotProps.data.contact }}
+          </router-link>
+        </template>
+      </Column>
+      <!-- Task status -->
+      <Column field="status" :header="$t('task.status')"></Column>
+      <!-- Goal date -->
+      <Column field="due_date" :header="$t('task.due_date')"></Column>
+      <!-- SLA -->
+      <Column field="SLA" header="SLA" class="sla">
+        <template #body="slotProps">
+          <div :class="slaClass(slotProps.data)">
+            {{ slotProps.data.SLA }}
+          </div>
+        </template>
+      </Column>
+      <!-- Owner -->
+      <Column field="owner" :header="$t('task.owner')"> </Column>
+      <!-- Customer -->
+      <Column field="customer" :header="$t('task.customer')">
+        <template #body="slotProps">
+          <router-link to="/foo" class="vertical-align-middle">{{
+            slotProps.data.customer
+          }}</router-link>
+        </template>
+      </Column>
+      <!-- Read -->
+      <Column field="call" :header="$t('task.call')"></Column>
+      <!-- Date opened -->
+      <Column field="open_date" :header="$t('task.open_date')"></Column>
+      <!-- Date closed -->
+      <Column field="close_date" :header="$t('task.close_date')"></Column>
+      <!-- Last changed -->
+      <Column field="last_change" :header="$t('modified_at')"></Column>
+      <!-- Notes -->
+      <Column field="remarks" :header="$t('task.notes')"></Column>
+    </DataTable>
+  </div>
 </template>
 
 <script setup>
-import { ref, onMounted, computed } from 'vue';
-import { TasksService } from '@/service/TasksService';
-import { useFormUtilsStore } from '@/stores/formUtils';
+import { ref, onMounted, computed } from "vue";
+import { TasksService } from "@/service/TasksService";
+import { useFormUtilsStore } from "@/stores/formUtils";
 
-import WMNewEntitySidebar from '@/components/layout/WMNewEntitySidebar.vue';
-import WMListSubHeader from '@/components/layout/WMListSubHeader.vue';
+import WMNewEntitySidebar from "@/components/layout/WMNewEntitySidebar.vue";
+import WMListSubHeader from "@/components/layout/WMListSubHeader.vue";
 
 onMounted(() => {
-    loading.value = true;
-    loadLazyData();
+  loading.value = true;
+  loadLazyData();
 });
 
 const loadLazyData = () => {
-    TasksService.getTasksFromApi({ page: lazyParams.value.page + 1 }).then((data) => {
-        console.log(data);
-        tasks.value = data.tasks;
-        totalRecords.value = data.totalRecords;
-        loading.value = false;
-    });
+  TasksService.getTasksFromApi({ page: lazyParams.value.page + 1 }).then(
+    (data) => {
+      console.log(data);
+      tasks.value = data.tasks;
+      totalRecords.value = data.totalRecords;
+      loading.value = false;
+    }
+  );
 };
 
 const onPage = (event) => {
-    lazyParams.value = event;
-    console.log(lazyParams)
-    loadLazyData();
+  lazyParams.value = event;
+  console.log(lazyParams);
+  loadLazyData();
 };
 
 const dt = ref();
@@ -107,54 +147,51 @@ const tasks = ref();
 const selectedTasks = ref([]);
 
 const isAnyRowSelected = computed(() => {
-    return selectedTasks?.value.length > 0;
+  return selectedTasks?.value.length > 0;
 });
 
 const metaKey = ref(true);
 
 const value = ref(null);
 const options = ref([
-    { name: 'כל אנשי הקשר', value: 2 },
-    { name: 'אנשי הקשר שלי', value: 1 },
+  { name: "כל אנשי הקשר", value: 2 },
+  { name: "אנשי הקשר שלי", value: 1 },
 ]);
-
 
 const formUtilsStore = useFormUtilsStore();
 
 const displayNewForm = () => {
-    formUtilsStore.expandSidebar = 'newTask';
-}
+  formUtilsStore.expandSidebar = "newTask";
+};
 
-const searchValue = ref('');
+const searchValue = ref("");
 
 const rowClass = (data) => {
-    console.log(data.is_open);
-    return [{ 'inactive_row': !data.is_open }];
+  console.log(data.is_open);
+  return [{ inactive_row: !data.is_open }];
 };
 
 const slaClass = (data) => {
-    return [
-
-        {
-            'bg-teal-200 text-teal-900': data.SLA === '10 ימים',
-            'bg-yellow-100 text-gray-900': data.SLA === '2 ימים',
-            'bg-red-100 text-red-600 ': data.SLA === '3 ימים',
-            'text-teal-900': data.SLA === 'עמד ביעד',
-            'text-red-600': data.SLA === 'הסתיים בחריגה',
-        }
-    ];
+  return [
+    {
+      "bg-teal-200 text-teal-900": data.SLA === "10 ימים",
+      "bg-yellow-100 text-gray-900": data.SLA === "2 ימים",
+      "bg-red-100 text-red-600 ": data.SLA === "3 ימים",
+      "text-teal-900": data.SLA === "עמד ביעד",
+      "text-red-600": data.SLA === "הסתיים בחריגה",
+    },
+  ];
 };
 
 const menuItems = [
-    { label: 'Whatsapp', value: 'option1' },
-    { label: 'SMS', value: 'option2' },
-]
+  { label: "Whatsapp", value: "option1" },
+  { label: "SMS", value: "option2" },
+];
 
 const filterLabels = [
-    { name: 'כל המשימות', value: 2 },
-    { name: 'המשימות שלי', value: 1 },
-]
-
+  { name: "כל המשימות", value: 2 },
+  { name: "המשימות שלי", value: 1 },
+];
 </script>
 
-<style scoped lang="scss"></style>                  
+<style scoped lang="scss"></style>
