ab268eb3c8275e0ee8205d2a986531dcf00f5dc3
Adrián Gámez <adrian@applicationcare.net>
2023-11-13 14:57:50 +0100

    adds calendar component

--- src/main.js
+++ src/main.js
@@ -1,106 +1,106 @@
-import { createApp, reactive, markRaw } from 'vue'
-import { createPinia } from 'pinia';
-import App from './App.vue'
+import { createApp, reactive, markRaw } from "vue";
+import { createPinia } from "pinia";
+import App from "./App.vue";
 // import 'maze-components/dist/index.css'
 
-import mazeComponents from 'maze-components'
-import PrimeVue from 'primevue/config';
-import router from './router';
+import mazeComponents from "maze-components";
+import PrimeVue from "primevue/config";
+import router from "./router";
 
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import AutoComplete from "primevue/autocomplete";
+import InputText from "primevue/inputtext";
+import Textarea from "primevue/textarea";
+import Dropdown from "primevue/dropdown";
+import Badge from "primevue/badge";
+import Divider from "primevue/divider";
+import Skeleton from "primevue/skeleton";
+import SelectButton from "primevue/selectbutton";
+import ScrollPanel from "primevue/scrollpanel";
+import BadgeDirective from "primevue/badgedirective";
+import Button from "primevue/button";
+import Menu from "primevue/menu";
+import ContextMenu from "primevue/contextmenu";
+import Card from "primevue/card";
+import Chart from "primevue/chart";
+import Chip from "primevue/chip";
+import Chips from "primevue/chips";
+import TabView from "primevue/tabview";
+import DataView from "primevue/dataview";
+import DataViewLayoutOptions from "primevue/dataviewlayoutoptions";
+import TabPanel from "primevue/tabpanel";
+import FileUpload from "primevue/fileupload";
+import Tag from "primevue/tag";
+import Steps from "primevue/steps";
+import Dialog from "primevue/dialog";
+import ConfirmDialog from "primevue/confirmdialog";
+import ConfirmationService from "primevue/confirmationservice";
+import Sidebar from "primevue/sidebar";
+import Password from "primevue/password";
+import Toast from "primevue/toast";
+import ToastService from "primevue/toastservice";
+import OverlayPanel from "primevue/overlaypanel";
+import Calendar from "primevue/calendar";
 
-import DataTable from 'primevue/datatable';
-import Column from 'primevue/column';
-import AutoComplete from 'primevue/autocomplete';
-import InputText from 'primevue/inputtext';
-import Textarea from 'primevue/textarea';
-import Dropdown from 'primevue/dropdown';
-import Badge from 'primevue/badge';
-import Divider from 'primevue/divider';
-import Skeleton from 'primevue/skeleton';
-import SelectButton from 'primevue/selectbutton';
-import ScrollPanel from 'primevue/scrollpanel';
-import BadgeDirective from 'primevue/badgedirective';
-import Button from 'primevue/button';
-import Menu from 'primevue/menu';
-import ContextMenu from 'primevue/contextmenu';
-import Card from 'primevue/card';
-import Chart from 'primevue/chart';
-import Chip from 'primevue/chip';
-import Chips from 'primevue/chips';
-import TabView from 'primevue/tabview';
-import DataView from 'primevue/dataview';
-import DataViewLayoutOptions from 'primevue/dataviewlayoutoptions';
-import TabPanel from 'primevue/tabpanel';
-import FileUpload from 'primevue/fileupload';
-import Tag from 'primevue/tag';
-import Steps from 'primevue/steps';
-import Dialog from 'primevue/dialog';
-import ConfirmDialog from 'primevue/confirmdialog';
-import ConfirmationService from 'primevue/confirmationservice';
-import Sidebar from 'primevue/sidebar';
-import Password from 'primevue/password';
-import Toast from 'primevue/toast';
-import ToastService from 'primevue/toastservice';
-import OverlayPanel from 'primevue/overlaypanel';
+import i18nInstance from "@/i18n";
 
-
-
-import i18nInstance from '@/i18n';
-
-
-import '@/assets/styles.scss';
-import '@/assets/themes/mytheme/theme.scss';
+import "@/assets/styles.scss";
+import "@/assets/themes/mytheme/theme.scss";
 
 const app = createApp(App)
-    .use(PrimeVue, { ripple: true })
-    .use(mazeComponents)
-    .use(ConfirmationService)
-    .use(ToastService)
-    .component('Column', Column)
-    .component('DataTable', DataTable)
-    .component('AutoComplete', AutoComplete)
-    .component('InputText', InputText)
-    .component('Textarea', Textarea)
-    .component('Dropdown', Dropdown)
-    .component('Badge', Badge)
-    .component('Divider', Divider)
-    .component('Skeleton', Skeleton)
-    .component('SelectButton', SelectButton)
-    .component('ScrollPanel', ScrollPanel)
-    .component('Button', Button)
-    .component('Menu', Menu)
-    .component('ContextMenu', ContextMenu)
-    .component('Card', Card)
-    .component('Chart', Chart)
-    .component('Chip', Chip)
-    .component('Chips', Chips)
-    .component('TabView', TabView)
-    .component('DataView', DataView)
-    .component('DataViewLayoutOptions', DataViewLayoutOptions)
-    .component('TabPanel', TabPanel)
-    .component('FileUpload', FileUpload)
-    .component('Tag', Tag)
-    .component('Steps', Steps)
-    .component('Sidebar', Sidebar)
-    .component('ConfirmDialog', ConfirmDialog)
-    .component('Dialog', Dialog)
-    .component('Password', Password)
-    .component('Toast', Toast)
-    .component('OverlayPanel', OverlayPanel);
+  .use(PrimeVue, { ripple: true })
+  .use(mazeComponents)
+  .use(ConfirmationService)
+  .use(ToastService)
+  .component("Column", Column)
+  .component("DataTable", DataTable)
+  .component("AutoComplete", AutoComplete)
+  .component("InputText", InputText)
+  .component("Textarea", Textarea)
+  .component("Dropdown", Dropdown)
+  .component("Badge", Badge)
+  .component("Divider", Divider)
+  .component("Skeleton", Skeleton)
+  .component("SelectButton", SelectButton)
+  .component("ScrollPanel", ScrollPanel)
+  .component("Button", Button)
+  .component("Menu", Menu)
+  .component("ContextMenu", ContextMenu)
+  .component("Card", Card)
+  .component("Chart", Chart)
+  .component("Chip", Chip)
+  .component("Chips", Chips)
+  .component("TabView", TabView)
+  .component("DataView", DataView)
+  .component("DataViewLayoutOptions", DataViewLayoutOptions)
+  .component("TabPanel", TabPanel)
+  .component("FileUpload", FileUpload)
+  .component("Tag", Tag)
+  .component("Steps", Steps)
+  .component("Sidebar", Sidebar)
+  .component("ConfirmDialog", ConfirmDialog)
+  .component("Dialog", Dialog)
+  .component("Password", Password)
+  .component("Toast", Toast)
+  .component("OverlayPanel", OverlayPanel)
+  .component("Calendar", Calendar);
 
-
-app.config.globalProperties.$appState = reactive({ isRTL: true, isNewThemeLoaded: false, layoutMode: 'light' });
-app.directive('badge', BadgeDirective);
+app.config.globalProperties.$appState = reactive({
+  isRTL: true,
+  isNewThemeLoaded: false,
+  layoutMode: "light",
+});
+app.directive("badge", BadgeDirective);
 app.use(i18nInstance);
 
-const pinia = createPinia()
+const pinia = createPinia();
 
 pinia.use(({ store }) => {
-    store.router = markRaw(router)
-})
+  store.router = markRaw(router);
+});
 
 app.use(pinia);
-app.use(router)
+app.use(router);
 
-app.mount('#app')
-
+app.mount("#app");
