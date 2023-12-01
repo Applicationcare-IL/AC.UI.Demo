af7a114d7387f7b64467bc6867da8f2eabef1e77
Adrián Gámez <adrian@applicationcare.net>
2023-12-01 11:41:09 +0100

    new project form refactor

---
+++ src/components/forms/projects/new/WMNewProjectFormGeneralDetails.vue
@@ -0,0 +1,46 @@
+<template>
+  <h1 class="h1 mb-0">{{ $t("new", ["project.project"]) }}</h1>
+  <h2 class="h2 my-0">{{ $t("general-details") }}</h2>
+  <div class="wm-form-row align-items-end gap-5">
+    <div class="wm-form-row gap-5">
+      <WMInput
+        name="owner"
+        type="info"
+        :highlighted="true"
+        :label="$t('owner') + ':'"
+        value="שרוליק כהן"
+      />
+      <WMInput
+        name="id"
+        type="info"
+        :highlighted="true"
+        :label="$t('team') + ':'"
+        value="שיפור פני העיר"
+      />
+    </div>
+  </div>
+  <div class="wm-form-row align-items-end gap-5">
+    <div class="wm-form-row gap-5">
+      <WMInput
+        name="project-name"
+        :required="true"
+        type="input-text"
+        :label="$t('project.project_name') + ':'"
+      />
+    </div>
+  </div>
+  <div class="wm-form-row align-items-end gap-5">
+    <div class="wm-form-row gap-5">
+      <WMInput
+        type="text-area"
+        :label="$t('project.project_description') + ':'"
+        id="description"
+        name="project-description"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup></script>
+
+<style scoped lang="scss"></style>
---
+++ src/components/forms/projects/new/WMNewProjectFormLocation.vue
@@ -0,0 +1,20 @@
+<template>
+  <h2 class="h2 mb-0">LOCATION</h2>
+  <div class="wm-form-row gap-4">
+    <WMSelectableButton label="city" v-model="showCityDataOptions" />
+    <WMSelectableButton label="address" v-model="showAddressOptions" />
+  </div>
+  <div class="wm-form-row gap-5">
+    <span v-if="showCityDataOptions"> CITY FIELDS </span>
+    <span v-if="showAddressOptions"> ADDRESS FIELDS </span>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const showCityDataOptions = ref(false);
+const showAddressOptions = ref(false);
+</script>
+
+<style scoped lang="scss"></style>
