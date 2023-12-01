af5316d6b1638d8c70605c0a56543b503a33c77d
Adrián Gámez <adrian@applicationcare.net>
2023-12-01 16:19:53 +0100

    more projects wip

---
+++ src/components/forms/projects/detail/WMDetailProjectFormTeamTab.vue
@@ -0,0 +1,15 @@
+<template>
+  <WMContactsTable :columns="contactColumns" :customerId="route.params.id" />
+</template>
+<script setup>
+import { ref } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+const { getContactColumns } = useListUtils();
+
+const contactColumns = ref(getContactColumns());
+</script>
+
+<style scoped lang="scss"></style>
