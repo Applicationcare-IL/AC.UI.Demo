c6ed73745994453d4316c5dbc345b4bc59aa9378
Adrián Gámez <adrian@applicationcare.net>
2023-12-26 18:12:22 +0100

    more translationsssssss

--- src/components/forms/tasks/detail/WMDetailTaskForm.vue
+++ src/components/forms/tasks/detail/WMDetailTaskForm.vue
@@ -126,7 +126,7 @@
                     name="recurring-task"
                     type="info"
                     :highlighted="true"
-                    label="Recurring task"
+                    :label="$t('task.recurring_task')"
                     value="No"
                   />
                 </div>
@@ -162,12 +162,7 @@
             <template #content>
               <div class="task-description flex flex-auto flex-column gap-5">
                 <div class="wm-form-row gap-5">
-                  <Textarea
-                    v-model="task.description"
-                    autoResize
-                    rows="5"
-                    disabled
-                  />
+                  <Textarea v-model="task.description" autoResize rows="5" disabled />
                 </div>
               </div>
             </template>
@@ -179,12 +174,7 @@
             <template #content>
               <div class="task-notes flex flex-auto flex-column gap-5">
                 <div class="wm-form-row gap-5">
-                  <WMInput
-                    :value="task.notes"
-                    type="text-area"
-                    id="notes"
-                    name="notes"
-                  />
+                  <WMInput :value="task.notes" type="text-area" id="notes" name="notes" />
                 </div>
               </div>
             </template>
@@ -252,24 +242,22 @@
           </TabPanel>
           <TabPanel header="טאב 1">
             <p>
-              Sed ut perspiciatis unde omnis iste natus error sit voluptatem
-              accusantium doloremque laudantium, totam rem aperiam, eaque ipsa
-              quae ab illo inventore veritatis et quasi architecto beatae vitae
-              dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit
-              aspernatur aut odit aut fugit, sed quia consequuntur magni dolores
-              eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci
-              velit, sed quia non numquam eius modi.
+              Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium
+              doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore
+              veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim
+              ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia
+              consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+              Consectetur, adipisci velit, sed quia non numquam eius modi.
             </p>
           </TabPanel>
           <TabPanel header="טאב 2">
             <p>
-              At vero eos et accusamus et iusto odio dignissimos ducimus qui
-              blanditiis praesentium voluptatum deleniti atque corrupti quos
-              dolores et quas molestias excepturi sint occaecati cupiditate non
-              provident, similique sunt in culpa qui officia deserunt mollitia
-              animi, id est laborum et dolorum fuga. Et harum quidem rerum
-              facilis est et expedita distinctio. Nam libero tempore, cum soluta
-              nobis est eligendi optio cumque nihil impedit quo minus.
+              At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis
+              praesentium voluptatum deleniti atque corrupti quos dolores et quas
+              molestias excepturi sint occaecati cupiditate non provident, similique sunt
+              in culpa qui officia deserunt mollitia animi, id est laborum et dolorum
+              fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero
+              tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus.
             </p>
           </TabPanel>
         </TabView>
