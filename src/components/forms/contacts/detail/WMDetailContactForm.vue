4aea628ceddc04f2e0cb8cc83dd6aa8541796529
Adrián Gámez <adrian@applicationcare.net>
2024-03-08 16:17:38 +0100

    milestone detail edit

--- src/components/forms/contacts/detail/WMDetailContactForm.vue
+++ src/components/forms/contacts/detail/WMDetailContactForm.vue
@@ -15,7 +15,7 @@
                     name="owner"
                     type="info"
                     :highlighted="true"
-                    :label="$t('owner') + ':'"
+                    :label="$t('owner.owner') + ':'"
                     :value="contact.owner.name"
                   />
                   <WMInput
