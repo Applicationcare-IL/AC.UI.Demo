308e0bc0531bf56a9661712313686e4e1a3d3f92
Adrián Gámez <adrian@applicationcare.net>
2024-03-26 12:46:32 +0100

    show attrbs in option sets

--- src/components/WMOptionSetValue.vue
+++ src/components/WMOptionSetValue.vue
@@ -1,5 +1,5 @@
 <template>
-  <span v-tooltip.top="optionSet.attributes">
+  <span v-tooltip.top="formattedAttributes" :class="classes">
     {{ translatedValue }}
   </span>
 </template>
@@ -8,7 +8,7 @@
 import { computed } from "vue";
 import { useI18n } from "vue-i18n";
 
-const { locale } = useI18n();
+const { locale, t } = useI18n();
 
 const props = defineProps({
   optionSet: {
@@ -17,8 +17,42 @@ const props = defineProps({
   },
 });
 
+const classes = computed(() => {
+  return {
+    "has-attributes": props.optionSet.attributes,
+  };
+});
+
 const translatedValue = computed(() => {
   if (!props.optionSet) return;
   return props.optionSet[`value_${locale.value}`];
 });
+
+function formatArray(array) {
+  return array
+    .map((item) => {
+      const [key, value] = item.split(": ");
+      return `${key.charAt(0).toUpperCase()}${key.slice(1)}: ${value}`;
+    })
+    .join("\n");
+}
+
+const formattedAttributes = computed(() => {
+  if (!props.optionSet.attributes) return;
+
+  const attributesWithTranslationKey = props.optionSet.attributes.map(
+    (attr) => {
+      return `${t(attr.translation_key)}: ${attr.value}`;
+    }
+  );
+
+  // print each attribute on a new line and stringify it
+  return formatArray(attributesWithTranslationKey);
+});
 </script>
+
+<style scoped lang="scss">
+.has-attributes {
+  cursor: help;
+}
+</style>
