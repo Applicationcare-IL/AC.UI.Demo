c54228d235aaae6460b761c901cfcf7599fea2af
Adrián Gámez <adrian@applicationcare.net>
2024-06-10 12:26:20 +0200

    remove maze-components from package.json

--- src/main.js
+++ src/main.js
@@ -3,11 +3,9 @@ import "@/assets/styles.scss";
 import "@/assets/themes/mytheme/theme.scss";
 
 import VueGoogleMaps from "@fawmi/vue-google-maps";
-// import 'maze-components/dist/index.css'
 import { createHead } from "@unhead/vue";
 import { QuillEditor } from "@vueup/vue-quill";
 import Echo from "laravel-echo";
-import mazeComponents from "maze-components";
 import { createPinia } from "pinia";
 import BadgeDirective from "primevue/badgedirective";
 import PrimeVue from "primevue/config";
--- src/router/index.js
+++ src/router/index.js
@@ -57,12 +57,6 @@ const router = createRouter({
           // component: () => import("@/views/user/Dashboard.vue"),
         },
         {
-          path: "/buttons",
-          name: "buttons",
-          exact: true,
-          component: () => import("@/views/Buttons.vue"),
-        },
-        {
           path: "/search/:query",
           name: "search",
           component: () => import("@/views/user/Search.vue"),
