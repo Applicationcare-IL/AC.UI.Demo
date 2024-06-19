2ac3cfde866222a0f0b2baa12c21e54ab7816ef3
Adrián Gámez <adrian@applicationcare.net>
2024-06-19 12:04:46 +0200

    WIP component

---
+++ src/components/WIP.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="flex align-items-start p-4 bg-yellow-100 border-round border-1 border-yellow-300">
+    <i class="pi pi-info-circle text-yellow-900 text-2xl mr-3"></i>
+    <div class="mr-3">
+      <div class="text-yellow-900 font-medium text-xl">This page is under construction</div>
+    </div>
+  </div>
+</template>
