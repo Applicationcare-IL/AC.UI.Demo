b43acc299a363259e17599b4db84961cab902363
Adrián Gámez <adrian@applicationcare.net>
2024-07-01 17:49:56 +0200

    add wminputsearch sizes to historie

--- histoire.setup.js
+++ histoire.setup.js
@@ -4,14 +4,23 @@ import "./histoire.css";
 
 import { defineSetupVue3 } from "@histoire/plugin-vue";
 import i18nInstance from "@/i18n";
+import { createPinia } from "pinia";
 
 // PRIMEVUE
 import PrimeVue from "primevue/config";
 import { reactive } from "vue";
 
+const pinia = createPinia();
+
 export const setupVue3 = defineSetupVue3(({ app }) => {
   app.use(PrimeVue);
   app.use(i18nInstance);
+
+  pinia.use(({ store }) => {
+    store.router = markRaw(router);
+  });
+
+  app.use(pinia);
 
   app.config.globalProperties.$appState = reactive({
     isRTL: true,
---
+++ src/components/forms/WMInputSearch.story.vue
@@ -0,0 +1,48 @@
+<template>
+  <Story title="WMInputSearch (Sizes)" :layout="{ type: 'grid', width: '100%' }">
+    <Variant title="Input text (xs - 60px)">
+      <WMInputSearch name="default" type="input-text" label="Label" size="xs" :options="options" />
+    </Variant>
+    <Variant title="Input text (sm - 152px)">
+      <WMInputSearch name="default" type="input-text" label="Label" size="sm" :options="options" />
+    </Variant>
+
+    <Variant title="Input text (md - 336px)">
+      <WMInputSearch name="default" type="input-text" label="Label" size="md" :options="options" />
+    </Variant>
+
+    <Variant title="Input text (lg - 520px)">
+      <WMInputSearch name="default" type="input-text" label="Label" size="lg" :options="options" />
+    </Variant>
+
+    <Variant title="Input text (xl - 704px)">
+      <WMInputSearch name="default" type="input-text" label="Label" size="xl" :options="options" />
+    </Variant>
+
+    <Variant title="Input text (full - 100%)">
+      <WMInputSearch
+        name="default"
+        type="input-text"
+        label="Label"
+        size="full"
+        :options="options"
+      />
+    </Variant>
+  </Story>
+</template>
+<script setup>
+import { ref } from "vue";
+
+const options = ref([
+  { name: "Option 1", value: "1" },
+  { name: "Option 2", value: "2" },
+  { name: "Option 3", value: "3" },
+  { name: "Option 4", value: "4" },
+  { name: "Option 5", value: "5" },
+  { name: "Option 6", value: "6" },
+  { name: "Option 7", value: "7" },
+  { name: "Option 8", value: "8" },
+  { name: "Option 9", value: "9" },
+  { name: "Option 10", value: "10" },
+]);
+</script>
