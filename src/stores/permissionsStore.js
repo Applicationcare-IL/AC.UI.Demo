1428f45f70d8b4a51345f0d20721973c93bb1686
Adrián Gámez <adrian@applicationcare.net>
2024-07-11 12:23:36 +0200

    fake budget-items.update permission

--- src/stores/permissionsStore.js
+++ src/stores/permissionsStore.js
@@ -6,6 +6,9 @@ export const usePermissionsStore = defineStore("permissions", {
   state: () => ({
     // Code managed permissions
     permissions: {
+      "budget-items": {
+        update: true,
+      },
       employees: {
         create: true,
       },
