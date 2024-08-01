461ca38d6b447b1402ff9977b63a04b081af01e0
JuanMurciaD <juan@applicationcare.net>
2024-08-01 13:42:41 +0200

    teams sidebar preview wip

---
+++ src/components/forms/teams/detail/WMTeamPreviewSidebar.vue
@@ -0,0 +1,56 @@
+<template>
+  <Sidebar
+      v-if="props.team"
+      v-model:visible="visible"
+      class="details-sidebar w-6"
+      :show-close-icon="false"
+      :class="''"
+      @update:model-value="updateModelValue"
+  >
+
+    <div class="wm-detail-form-container flex flex-auto flex-column overflow-auto">
+      <div class="flex justify-content-between">
+
+        <pre>{{ props.team }}</pre>
+
+      </div>
+    </div>
+
+  </Sidebar>
+</template>
+
+<script setup>
+// IMPORTS
+import  { ref } from 'vue';
+
+// DEPENDENCIES
+
+// INJECT
+
+// PROPS, EMITS
+const props = defineProps({
+  isVisible: {
+    type: Boolean,
+    default: false,
+  },
+  team: {
+    type: Object,
+    default: () => {},
+  },
+});
+
+// REFS
+const visible = ref(false);
+
+// COMPUTED
+
+// COMPONENT METHODS AND LOGIC
+
+// PROVIDE, EXPOSE
+
+// WATCHERS
+
+// LIFECYCLE METHODS (https://vuejs.org/api/composition-api-lifecycle.html)
+</script>
+
+<style scoped></style>
