c679a21d8e2e661dfe1de22719883877f5bbdbe1
Adrián Gámez <adrian@applicationcare.net>
2024-08-09 09:08:29 +0200

    edit config when project is pending configuration WIP

--- src/components/forms/projects/detail/WMDetailProjectFormClassification.vue
+++ src/components/forms/projects/detail/WMDetailProjectFormClassification.vue
@@ -4,49 +4,59 @@
     <template #content>
       <div class="wm-form-row gap-5">
         <WMInputSearch
+          v-model="selectedProjectType"
           name="project_type"
           :highlighted="true"
           :required="true"
           :label="$t('project.project_type') + ':'"
           type="input-search"
           width="152"
+          :options="projectTypes"
           :placeholder="$t('project.project_types_placeholder')"
-          v-model="selectedProjectType"
           :option-set="true"
-          :disabled="true"
+          :disabled="isDisabled"
         />
 
         <WMInputSearch
+          v-model="selectedProjectArea"
           name="project_area"
           :highlighted="true"
           :label="$t('project.project_area') + ':'"
           width="152"
+          :options="projectAreas"
           :placeholder="$t('project.project_areas_placeholder')"
-          @change="filterProjectDetailsDropdown($event.value.id)"
-          v-model="selectedProjectArea"
           :option-set="true"
-          :disabled="true"
+          :disabled="isDisabled"
+          @change="filterProjectDetailsDropdown($event.value.id)"
         />
 
         <WMInputSearch
+          v-model="selectedProjectDetail"
           name="project_detail"
           :highlighted="true"
           :label="$t('project.project_detail') + ':'"
           width="152"
+          :options="projectDetails"
           :placeholder="$t('project.project_details_placeholder')"
-          v-model="selectedProjectDetail"
           :option-set="true"
-          :disabled="true"
+          :disabled="isDisabled"
         />
       </div>
     </template>
   </Card>
 </template>
 <script setup>
-import { ref, onMounted } from "vue";
+// IMPORTS
+import { computed, onMounted, ref } from "vue";
 
 import { useOptionSetsStore } from "@/stores/optionSets";
 
+// DEPENDENCIES
+const optionSetsStore = useOptionSetsStore();
+
+// INJECT
+
+// PROPS, EMITS
 const props = defineProps({
   project: {
     type: Object,
@@ -54,32 +64,51 @@ const props = defineProps({
   },
 });
 
-const optionSetsStore = useOptionSetsStore();
+// REFS
+const projectTypes = ref([]);
+const projectAreas = ref([]);
+const projectDetails = ref([]);
 
 const selectedProjectType = ref();
 const selectedProjectArea = ref();
 const selectedProjectDetail = ref();
 
+// COMPUTED
+const isDisabled = computed(() => {
+  return props.project.status.value === "pending_configuration" ? false : true;
+});
+
+// COMPONENT METHODS AND LOGIC
+
+// PROVIDE, EXPOSE
+
+// WATCHERS
+
+// LIFECYCLE METHODS (https://vuejs.org/api/composition-api-lifecycle.html)
 onMounted(() => {
   optionSetsStore.getOptionSetValuesFromApiRaw("project_type").then((data) => {
+    projectTypes.value = data;
+
     selectedProjectType.value = data.find((item) => {
       return item.value === props.project.project_type.value;
     });
   });
 
   optionSetsStore.getOptionSetValuesFromApiRaw("project_area").then((data) => {
+    projectAreas.value = data;
+
     selectedProjectArea.value = data.find((item) => {
       return item.value === props.project.project_area?.value;
     });
   });
 
-  optionSetsStore
-    .getOptionSetValuesFromApiRaw("project_detail")
-    .then((data) => {
-      selectedProjectDetail.value = data.find((item) => {
-        return item.value === props.project.project_detail?.value;
-      });
+  optionSetsStore.getOptionSetValuesFromApiRaw("project_detail").then((data) => {
+    projectDetails.value = data;
+
+    selectedProjectDetail.value = data.find((item) => {
+      return item.value === props.project.project_detail?.value;
     });
+  });
 });
 </script>
 
