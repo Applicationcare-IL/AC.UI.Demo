c2f50adfc57f2d0efc79ce287c9fa78a5e57e908
Adrián Gámez <adrian@applicationcare.net>
2024-08-20 11:10:36 +0200

    format start date in new message

--- src/composables/useAdminMessages.js
+++ src/composables/useAdminMessages.js
@@ -1,71 +1,73 @@
-import { useAdminMessagesStore} from "@/stores/adminMessagesStore";
+import { useAdminMessagesStore } from "@/stores/adminMessagesStore";
 
 const useAdminFlowmaze = () => {
-    const adminMessagesStore = useAdminMessagesStore();
+  const { formatDateToAPI } = useDates();
+  const adminMessagesStore = useAdminMessagesStore();
 
-    // ACTIONS
-    const getMessages = async (params) => {
-        const response = await adminMessagesStore.getMessages(params);
-        const messages = response.data.map((message) => mapMessage(message));
-        const totalRecords = response.meta.total;
+  // ACTIONS
+  const getMessages = async (params) => {
+    const response = await adminMessagesStore.getMessages(params);
+    const messages = response.data.map((message) => mapMessage(message));
+    const totalRecords = response.meta.total;
 
-        return { data: messages, totalRecords };
-    };
+    return { data: messages, totalRecords };
+  };
 
-    const getMessage = async (id) => {
-        const response = await adminMessagesStore.getMessage(id);
+  const getMessage = async (id) => {
+    const response = await adminMessagesStore.getMessage(id);
 
-        return mapMessage(response.data);
-    }
+    return mapMessage(response.data);
+  };
 
-    const createMessage = async (message) => {
-        return await adminMessagesStore.createMessage(message);
-    }
+  const createMessage = async (message) => {
+    return await adminMessagesStore.createMessage(message);
+  };
 
-    const updateMessage = async (messageId, data) => {
-        return await adminMessagesStore.updateMessage(messageId, data);
-    }
+  const updateMessage = async (messageId, data) => {
+    return await adminMessagesStore.updateMessage(messageId, data);
+  };
 
-    const mapMessage = (message) => {
-        return {
-            ...message,
-            link_detail: {
-                text: message.id,
-                id: message.id,
-            },
-            title: message.topic,
-            target: 'services',
-            //fake state to be able to show something in the list
-            state: {
-                id: 291,
-                value: "active",
-                value_en: "Active",
-                value_he: "פעיל",
-                attributes: []
-            },
-        }
-    };
-
-    const parseMessage = (message) => {
-        return {
-            ...message,
-            important: message.important.value,
-            teams_id: message.teams_id.map((team) => team.id),
-            roles_id: message.roles_id.map((role) => role.id),
-            users_id: message.users_id.map((user) => user.id),
-        }
-    }
-
+  const mapMessage = (message) => {
     return {
-        // ACTIONS
-        getMessages,
-        getMessage,
-        createMessage,
-        updateMessage,
-        //UTILITIES
-        mapMessage,
-        parseMessage,
-    }
-}
+      ...message,
+      link_detail: {
+        text: message.id,
+        id: message.id,
+      },
+      title: message.topic,
+      target: "services",
+      //fake state to be able to show something in the list
+      state: {
+        id: 291,
+        value: "active",
+        value_en: "Active",
+        value_he: "פעיל",
+        attributes: [],
+      },
+    };
+  };
+
+  const parseMessage = (message) => {
+    return {
+      ...message,
+      important: message.important.value,
+      teams_id: message.teams_id.map((team) => team.id),
+      roles_id: message.roles_id.map((role) => role.id),
+      users_id: message.users_id.map((user) => user.id),
+      start_date: formatDateToAPI(message.start_date),
+    };
+  };
+
+  return {
+    // ACTIONS
+    getMessages,
+    getMessage,
+    createMessage,
+    updateMessage,
+    //UTILITIES
+    mapMessage,
+    parseMessage,
+  };
+};
 
 export default useAdminFlowmaze;
