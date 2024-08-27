a34e0d80b8df7ba534b01238eb1f991d50c3af89
JuanMurciaD <juan@applicationcare.net>
2024-08-27 11:34:23 +0200

    add unlock button user detail

--- src/composables/useAdminUsers.js
+++ src/composables/useAdminUsers.js
@@ -58,6 +58,7 @@ const useAdminFlowmaze = () => {
       },
       title: username,
       manager_fullname: user.manager?.name && user.manager?.surname ? manager_fullname : "",
+      locked: !user.locked,
     };
   };
 
--- src/views/admin/forms/detail/UserDetail.vue
+++ src/views/admin/forms/detail/UserDetail.vue
@@ -24,7 +24,7 @@
     <template #custom-buttons>
       <div class="flex gap-3">
         <WMResetPassAdminUsersButton :selected-users="selectedUsers" />
-        <WMUnlockAdminUserButton :selected-users="selectedUsers"/>
+        <WMUnlockAdminUserButton :selected-users="selectedUsers" :isDisabled="user.locked"/>
       </div>
     </template>
   </WMDetailFormSubHeader>
