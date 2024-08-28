8b4c1ca10bfa06e9da0ab523bf965bc822d16c71
Adrián Gámez <adrian@applicationcare.net>
2024-08-28 13:09:28 +0200

    message target projects

--- src/components/forms/messages/shared/WMMessageTargetProjects.vue
+++ src/components/forms/messages/shared/WMMessageTargetProjects.vue
@@ -1,50 +1,50 @@
 <template>
   <div class="flex flex-column gap-3">
     <WMInputSearch
-        v-model="selectedProjectType"
-        name="project_type"
-        :highlighted="true"
-        :required="true"
-        :label="$t('message.project-type') + ':'"
-        :options="projectTypes"
-        size="md"
-        :placeholder="$t('message.select-project-type')"
-        :option-set="true"
-        @change="handleSelectedProjectTypeChange"
+      v-model="selectedProjectType"
+      name="project_type"
+      :highlighted="true"
+      :required="true"
+      :label="$t('message.project-type') + ':'"
+      :options="projectTypes"
+      size="md"
+      :placeholder="$t('message.select-project-type')"
+      :option-set="true"
+      @change="handleSelectedProjectTypeChange"
     />
 
     <WMInputSearch
-        v-model="selectedProjectArea"
-        name="project_area"
-        :highlighted="true"
-        :label="$t('message.project-area') + ':'"
-        :options="projectAreas"
-        size="md"
-        :placeholder="$t('message.select-project-area')"
-        :option-set="true"
-        :disabled="isProjectAreaEmpty"
-        @change="handleSelectedProjectAreaChange"
+      v-model="selectedProjectArea"
+      name="project_area"
+      :highlighted="true"
+      :label="$t('message.project-area') + ':'"
+      :options="projectAreas"
+      size="md"
+      :placeholder="$t('message.select-project-area')"
+      :option-set="true"
+      :disabled="isProjectTypeEmpty"
+      @change="handleSelectedProjectAreaChange"
     />
 
     <WMInputSearch
-        v-model="selectedProjectDetail"
-        name="project_detail"
-        :highlighted="true"
-        :label="$t('message.project-detail') + ':'"
-        :options="projectDetails"
-        size="md"
-        :placeholder="$t('message.select-project-detail')"
-        :option-set="true"
-        :disabled="isProjectDetailEmpty"
+      v-model="selectedProjectDetail"
+      name="project_detail"
+      :highlighted="true"
+      :label="$t('message.project-detail') + ':'"
+      :options="projectDetails"
+      size="md"
+      :placeholder="$t('message.select-project-detail')"
+      :option-set="true"
+      :disabled="isProjectAreaEmpty"
     />
   </div>
 </template>
 
 <script setup>
 // IMPORTS
-import {onMounted, ref} from "vue";
+import { computed, onMounted, ref } from "vue";
 
-import {useOptionSetsStore} from "@/stores/optionSets";
+import { useOptionSetsStore } from "@/stores/optionSets";
 
 // DEPENDENCIES
 const optionSetsStore = useOptionSetsStore();
@@ -52,6 +52,12 @@ const optionSetsStore = useOptionSetsStore();
 // INJECT
 
 // PROPS, EMITS
+const props = defineProps({
+  message: {
+    type: Object,
+    default: () => ({}),
+  },
+});
 
 // REFS
 const projectTypes = ref([]);
@@ -62,55 +68,53 @@ const selectedProjectType = ref(false);
 const selectedProjectArea = ref(false);
 const selectedProjectDetail = ref(false);
 
-const isProjectAreaEmpty = ref(true);
-const isProjectDetailEmpty = ref(true);
-
 // COMPUTED
+const isProjectTypeEmpty = computed(() => !selectedProjectType.value);
+const isProjectAreaEmpty = computed(() => !selectedProjectArea.value);
 
 // COMPONENT METHODS AND LOGIC
 const loadLazyData = () => {
   optionSetsStore
-      .getOptionSetValuesFromApiRaw("project_type")
-      .then((data) => (projectTypes.value = data));
-}
+    .getOptionSetValuesFromApiRaw("project_type")
+    .then((data) => (projectTypes.value = data));
+};
 
 const filterProjectAreasDropdown = (value) => {
   optionSetsStore
-      .getOptionSetValuesFromApiRaw("project_area", value)
-      .then((data) => (projectAreas.value = data));
-  isProjectAreaEmpty.value = false;
+    .getOptionSetValuesFromApiRaw("project_area", value)
+    .then((data) => (projectAreas.value = data));
 };
 
 const filterProjectDetailsDropdown = (value) => {
   optionSetsStore
-      .getOptionSetValuesFromApiRaw("project_detail", value)
-      .then((data) => (projectDetails.value = data));
-  isProjectDetailEmpty.value = false;
+    .getOptionSetValuesFromApiRaw("project_detail", value)
+    .then((data) => (projectDetails.value = data));
 };
 
 const clearProjectAreasDropdown = () => {
-  projectAreas.value = []
+  projectAreas.value = [];
   selectedProjectArea.value = null;
-  isProjectAreaEmpty.value = true;
 };
 
 const clearProjectDetailsDropdown = () => {
   projectDetails.value = [];
   selectedProjectDetail.value = null;
-  isProjectDetailEmpty.value = true;
 };
 
 const handleSelectedProjectTypeChange = (option) => {
   if (!option) {
+    selectedProjectType.value = null;
     clearProjectAreasDropdown();
     clearProjectDetailsDropdown();
     return;
   }
+
   filterProjectAreasDropdown(option.value.id);
 };
 
 const handleSelectedProjectAreaChange = (option) => {
   if (!option) {
+    selectedProjectArea.value = null;
     clearProjectDetailsDropdown();
     return;
   }
@@ -122,8 +126,14 @@ const handleSelectedProjectAreaChange = (option) => {
 // WATCHERS
 
 // LIFECYCLE METHODS (https://vuejs.org/api/composition-api-lifecycle.html)
-onMounted(() => {
-  loadLazyData();
+onMounted(async () => {
+  await loadLazyData();
+
+  if (props.message) {
+    selectedProjectType.value = props.message.project_type_id;
+    selectedProjectArea.value = props.message.project_area_id;
+    selectedProjectDetail.value = props.message.project_detail_id;
+  }
 });
 </script>
 
